63523cb82040d8d0b15b6f802c1e3a8161a7cf46
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-03-27 14:06:10 -0300

    Controller Base

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,23 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+use View;
+use Auth;
+
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function callAction($method, $parameters)
+	{
+        /*
+		View::share('company', array(
+            'name'=> config('app.name', 'Laravel'),
+        ));
+        */
+		View::share('user', Auth::user());
+
+		return parent::callAction($method, $parameters);
+	}
 }
