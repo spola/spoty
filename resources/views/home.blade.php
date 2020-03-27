0f1244e1b03325104b16a8720b44120f8f8ae815
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-03-27 14:52:07 -0300

    Calendario en el home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,5 +18,13 @@
             </div>
         </div>
     </div>
-</div>
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=America%2FSantiago&amp;src=MzhiMDIwYWpjdGo4Mm02bjdqZ3I4cjc1YzRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZXMuY2wjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%237CB342&amp;color=%23009688&amp;mode=WEEK&amp;showTitle=0&amp;showCalendars=0&amp;showTabs=1&amp;showDate=1&amp;showTz=0" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+        </div>
+    </div>
+
+
+
+
 @endsection
