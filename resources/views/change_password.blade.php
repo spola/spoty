9fa4dd64f6f958a1cf13516012294544c72202c6
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-03-27 23:59:27 -0300

    Cambio de texto de cancelar a volver

--- resources/views/change_password.blade.php
+++ resources/views/change_password.blade.php
@@ -47,7 +47,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <a href="{{url('/')}}" class="btn btn-default">
-                                    Cancelar
+                                    Volver
                                 </a>
                                 <button type="submit" class="btn btn-primary">
                                     Guardar
