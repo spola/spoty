c0c9ae69cab1c18a55808ef8758e1c8463d2afca
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-03-27 21:19:11 -0300

    Mover archivos

--- resources/views/course/show.blade.php
+++ resources/views/course/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app_students')
 
 @section('content')
 <section class="course">
--- resources/views/students/home.blade.php
+++ resources/views/students/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app_students')
 
 @section('content')
     <div class="row justify-content-center">
