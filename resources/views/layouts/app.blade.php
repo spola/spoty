7c8d9ba15cca7cb56318b14a9314771b219374ed
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-03-27 21:14:14 -0300

    Menú cambio de contraseña

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -103,6 +103,9 @@
                                     </a>
 
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}">
+                                            Cambiar Contraseña
+                                        </a>
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                                         document.getElementById('logout-form').submit();">
