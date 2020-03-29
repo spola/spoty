cfb562721b10cbe490307c9ab808c9d76d458e35
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-03-29 16:38:01 -0300

    No  aplicar csrf al logout

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Contracts\Encryption\Encrypter;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
@@ -21,4 +23,20 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @param  \Illuminate\Contracts\Encryption\Encrypter  $encrypter
+     * @return void
+     */
+    public function __construct(Application $app, Encrypter $encrypter)
+    {
+        parent::__construct($app, $encrypter);
+        $this->except = [
+            route('logout')
+        ];
+    }
 }
