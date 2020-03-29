5e21345afcbcaf0c09e313cfc55eafd6c81655b4
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-03-29 14:34:14 -0300

    Dashboard para padres

--- app/Http/Controllers/Parents/HomeController.php
+++ app/Http/Controllers/Parents/HomeController.php
@@ -7,13 +7,58 @@ use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+	
+	/**
+	* Show the application dashboard.
+    *
+    * @return \Illuminate\Contracts\Support\Renderable
+    */
     public function index()
     {
-        return view('parents/home');
-    }
+		$user = \Auth::user();
+		
+		$respuesta = [];
+		
+		foreach($user->childrens as $student) {
+			$queryStr = "SELECT c.id, c.name, count(1) as total, sum(case when scored = 1 then 1 else 0 end) as total_evaluadas
+                    from activities a
+                        join courses c on a.course_id  = c.id 
+                    where c.grade_id = :grade_id
+                    group by c.id, c.name";
+			
+			$totales = \DB::select( \DB::raw($queryStr), [
+						            'grade_id' => $student->grade_id
+			        ]);
+			$queryStr = "SELECT c.id, c.name, count(1) as total, sum(case when scored = 1 then 1 else 0 end) as total_evaluadas
+                    from activities a
+                        join courses c on a.course_id  = c.id 
+                        join user_activities ua on a.id = ua.activity_id 
+                    where c.grade_id = :grade_id 
+                        and ua.user_id = :user
+                        and ua.deleted_at is null
+                    group by c.id, c.name
+                    order by c.id";
+			$respondidasDB = \DB::select( \DB::raw($queryStr), [
+			            'grade_id' => $student->grade_id,
+			            'user' => $student->id,
+			        ]);
+			$respondidas = [];
+			foreach($respondidasDB as $respondida) {
+				$respondidas[$respondida->id] = $respondida;
+			}
+			foreach($totales as $total) {
+				$resp = isset($respondidas[$total->id])?$respondidas[$total->id]:null;
+				$total->total_evaluadas = (int) $total->total_evaluadas;
+				$total->respondidas_total = $resp==null? 0 : (int)$resp->total;
+				$total->respondidas_total_evaluadas = $resp==null? 0 : (int)$resp->total_evaluadas;
+			}
+			$respuesta[] = [
+				'student' => $student,
+                'totales' => $totales
+            ];
+		}
+		return view('parents/home', [
+		            'respuestas' => $respuesta
+		        ]);
+	}
 }
--- resources/views/parents/home.blade.php
+++ resources/views/parents/home.blade.php
@@ -21,8 +21,91 @@
     </div>
     */
     ?>
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-        </div>
-    </div>
+<style>
+.order-card {
+    color: #fff;
+}
+
+.bg-c-blue {
+    background: linear-gradient(45deg,#4099ff,#73b4ff);
+}
+
+.bg-c-green {
+    background: linear-gradient(45deg,#2ed8b6,#59e0c5);
+}
+
+.bg-c-yellow {
+    background: linear-gradient(45deg,#FFB64D,#ffcb80);
+}
+
+.bg-c-pink {
+    background: linear-gradient(45deg,#FF5370,#ff869a);
+}
+.card {
+    border-radius: 5px;
+    -webkit-box-shadow: 0 1px 2.94px 0.06px rgba(4,26,55,0.16);
+    box-shadow: 0 1px 2.94px 0.06px rgba(4,26,55,0.16);
+    border: none;
+    margin-bottom: 30px;
+    -webkit-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+}
+
+.card .card-block {
+    padding: 25px;
+}
+
+.order-card i {
+    font-size: 26px;
+}
+
+.f-left {
+    float: left;
+}
+
+.f-right {
+    float: right;
+}
+    </style>
+
+<div class="container">
+    
+        @foreach($respuestas as $respuesta)
+            <?php $student = $respuesta['student']; $totales = $respuesta['totales'] ?>
+            <div class="row">
+                <h1>{{$student->name}} <small><a href="{{route('parents.grade.show', ['grade'=>$student->grade_id])}}">(detalle)</a></small> </h1>
+            </div>
+            <div class="row">
+            @foreach($totales as $course)
+                <div class="col-md-3 col-xl-2">
+                    <div class="card order-card {{ $course->respondidas_total_evaluadas == $course->total_evaluadas ? 'bg-c-green':'bg-c-blue' }} ">
+                        <div class="card-block">
+                            <h3 class="m-b-20">
+                                {{$course->name}}
+                            </h3>
+                            <h5 class="text-right">
+                                Evaluadas {{$course->respondidas_total_evaluadas}}/{{$course->total_evaluadas}}
+                            </h5>
+                            <p class="m-b-0 text-right">Todas &nbsp;
+                                <span class="f-right">
+                                {{$course->respondidas_total}}/{{$course->total}}
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            </div>
+        @endforeach
+    
+</div>
+
 @endsection
+<?php 
+/*
+dd([
+    'totales' => $totales,
+    'respondidas' => $respondidas
+]);
+*/
+?>
