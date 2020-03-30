60e7cffae8638180f7446a1d33bf4cddc8533a81
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-03-30 02:02:20 -0300

    BugFix: Personas con varios cursos

--- resources/views/parents/calendars.blade.php
+++ resources/views/parents/calendars.blade.php
@@ -8,7 +8,7 @@
         <ul class="nav nav-tabs" id="myTab" role="tablist">
             @foreach($grades as $grade)
             <li class="nav-item">
-                <a class="nav-link active" id="{{$grade->name}}-tab" data-toggle="tab" href="#{{$grade->name}}" role="tab" aria-controls="{{$grade->name}}" aria-selected="true">{{$grade->name}}</a>
+                <a class="nav-link active" id="{{$grade->id}}-tab" data-toggle="tab" href="#{{$grade->id}}" role="tab" aria-controls="{{$grade->name}}" aria-selected="true">{{$grade->name}}</a>
             </li>
             @endforeach
         </ul>
@@ -19,7 +19,7 @@
     <div class="col-md-12">
         <div class="tab-content" id="myTabContent">
             @foreach($grades as $grade)
-            <div class="tab-pane fade show active" id="{{$grade->name}}" role="tabpanel" aria-labelledby="{{$grade->name}}-tab">
+            <div class="tab-pane fade show active" id="{{$grade->id}}" role="tabpanel" aria-labelledby="{{$grade->id}}-tab">
                 <iframe src="{{$grade->calendar}}" seamless="seamless" style="display:block; width:100%; height:100vh;" frameborder="0" scrolling="no"></iframe>
             </div>
             @endforeach
