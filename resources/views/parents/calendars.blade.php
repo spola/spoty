ad96a1860aa43572ff82e31d08f25a0828e32d6a
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-03-30 02:36:35 -0300

    BugFix: Multiples Calendarios

--- resources/views/parents/calendars.blade.php
+++ resources/views/parents/calendars.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.app_parents')
 
 @section('content')
-<?php //dd($grades) ?>
 
 <div class="row justify-content-center">
     <div class="col-md-12">
         <ul class="nav nav-tabs" id="myTab" role="tablist">
-            @foreach($grades as $grade)
+            @foreach($grades as $k => $grade)
             <li class="nav-item">
-                <a class="nav-link active" id="{{$grade->id}}-tab" data-toggle="tab" href="#{{$grade->id}}" role="tab" aria-controls="{{$grade->name}}" aria-selected="true">{{$grade->name}}</a>
+                <a class="nav-link {{$k == 0?'active':''}}" id="tab{{$grade->id}}" data-toggle="tab" href="#content{{$grade->id}}" role="tab" aria-controls="content{{$grade->id}}" aria-selected="true">{{$grade->name}}</a>
             </li>
             @endforeach
         </ul>
@@ -18,8 +17,8 @@
 <div class="row justify-content-center course">
     <div class="col-md-12">
         <div class="tab-content" id="myTabContent">
-            @foreach($grades as $grade)
-            <div class="tab-pane fade show active" id="{{$grade->id}}" role="tabpanel" aria-labelledby="{{$grade->id}}-tab">
+            @foreach($grades as $k => $grade)
+            <div class="tab-pane fade show {{$k == 0?'active':''}}" id="content{{$grade->id}}" role="tabpanel" aria-labelledby="tab{{$grade->id}}">
                 <iframe src="{{$grade->calendar}}" seamless="seamless" style="display:block; width:100%; height:100vh;" frameborder="0" scrolling="no"></iframe>
             </div>
             @endforeach
