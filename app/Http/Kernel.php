b10b31b7931470f7d3eb61556f9047f8bba1c4bc
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-04-01 11:13:20 -0300

    Middleware para validar que que sea un administrador de cursos

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,6 +58,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'auth.administration' => \App\Http\Middleware\OnlyAdministratorIfAuthenticated::class,
+        'auth.administration.grade' => \App\Http\Middleware\OnlyGradeAdministratorIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
