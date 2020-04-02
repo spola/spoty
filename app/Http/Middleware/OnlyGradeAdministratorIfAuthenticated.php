c9154013d2cf6b39cc3e81b742d6597316202f92
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-04-01 23:41:58 -0300

    Middleware con validación de si usuario es administrador del curso

--- app/Http/Middleware/OnlyGradeAdministratorIfAuthenticated.php
+++ app/Http/Middleware/OnlyGradeAdministratorIfAuthenticated.php
@@ -18,10 +18,29 @@ class OnlyGradeAdministratorIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         $user = Auth::user();
+        $grade = $request->route('grade');
 
-        $cant = $user->adminGrades()->count();
+        $grade = isset($grade)? ( is_numeric($grade)? (int)$grade : $grade->id ) : null;
+/*
+        \DB::listen(function($query) {
+            dd([
+                'sql' => $query->sql,
+                'bindings' => $query->bindings,
+                'time' =>$query->time
+            ]);
+        });
+*/
+//dd($grade);
+
+        if(!isset($grade)) {
+            dd("redirigiendo por falta de grade");
+            return redirect('/');
+        }
+
+        $cant = $user->adminGrades()->where('grade_id', $grade)->count();
 
         if ( $user == null || $cant < 1 ) {
+            dd("No hay grados suficientes");
             return redirect('/');
         }
 
