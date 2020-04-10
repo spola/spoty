a3b93a28db851076cd4172c19fd35acfce8f630c
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-04-10 15:24:56 -0400

    sección de styles en el layout

--- resources/views/layouts/app_parents.blade.php
+++ resources/views/layouts/app_parents.blade.php
@@ -30,6 +30,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/fontawesome.min.js" integrity="sha256-NP9NujdEzS5m4ZxvNqkcbxyHB0dTRy9hG13RwTVBGwo=" crossorigin="anonymous"></script>
     <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
 
+    @yield("styles")
 </head>
 <body>
     <div class="wrapper">
