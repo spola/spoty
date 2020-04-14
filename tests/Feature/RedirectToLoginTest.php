62abbfe496b5fea56fc341a5a494289fe5f70989
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-04-14 17:33:34 -0400

    Test: Redirect to login when visiting /

--- tests/Feature/RedirectToLoginTest.php
+++ tests/Feature/RedirectToLoginTest.php
@@ -5,17 +5,17 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class RedirectToLoginTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testRedirectoDefaultToLogin()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $response = $this
+        ->get('/')
+        ->assertRedirect('login');;
     }
 }
