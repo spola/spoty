fd97a4653fa6751170681177830c3e217505970b
Sergio Pola <sergio.pola.contreras@gmail.com>
2020-04-22 00:30:51 -0400

    Configuración de passport

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,17 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::group([ 'prefix' => 'auth'], function (){
+    Route::group(['middleware' => ['guest:api']], function () {
+        Route::post('login', 'API\AuthController@login');
+        Route::post('signup', 'API\AuthController@signup');
+    });
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('logout', 'API\AuthController@logout');
+        Route::get('getuser', 'API\AuthController@getUser');
+    });
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
